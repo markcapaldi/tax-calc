5213cfcd0cfe43bef8df0a91048f66adef97ad88
Leon Larin <leonlarin@gmail.com>
2019-05-06 21:18:29 +0100

    Added IncomeTaxData class and defaults file

--- CalculateTax.py
+++ CalculateTax.py
@@ -2,6 +2,9 @@
 
 import argparse
 import logging
+import json
+
+from defaults import DEFAULT_DATA, TAX_DATA_FILE_NAME
 
 
 help_text = '''Calculate the amount of Income tax due in a given tax year
@@ -16,6 +19,50 @@ args = parser.parse_args()
 
 if args.verbose:
     logging.basicConfig(level=logging.DEBUG)
+
+
+def _reset_data():
+    """populate JSON file with default data"""
+    logging.debug('Reseting the Income Tax Data from defaults.')
+    with open(TAX_DATA_FILE_NAME, 'w') as outfile:
+        json.dump(DEFAULT_DATA, outfile)
+
+
+class IncomeTaxData:
+
+    STARTER_RATE = 'starter_rate'
+    BASIC_RATE = 'basic_rate'
+    INTERMEDIATE_RATE = 'intermediate_rate'
+    HIGHER_RATE = 'higher_rate'
+    TOP_RATE = 'top_rate'
+
+    BANDS_LABELS = (
+        (STARTER_RATE, 'Starter Rate'),
+        (BASIC_RATE, 'Basic Rate'),
+        (INTERMEDIATE_RATE, 'Intermediate Rate'),
+        (HIGHER_RATE, 'Higher Rate'),
+        (TOP_RATE, 'Top Rate'),
+    )
+
+    def __init__(self, year):
+        with open(TAX_DATA_FILE_NAME) as json_file:
+            data = json.load(json_file)
+
+        try:
+            year_data = data[year]
+        except KeyError:
+            logging.info(
+                'Income Tax Data for year {year} is not available.', year
+            )
+            return None
+
+        self.personal_allowance = year_data['personal_allowance']
+
+        self.starter_rate = year_data[self.STARTER_RATE]
+        self.basic_rate = year_data[self.BASIC_RATE]
+        self.intermediate_rate = year_data[self.INTERMEDIATE_RATE]
+        self.higher_rate = year_data[self.HIGHER_RATE]
+        self.top_rate = year_data[self.TOP_RATE]
 
 
 def main():
---
+++ defaults.py
@@ -0,0 +1,80 @@
+TAX_DATA_FILE_NAME = "tax_data.json"
+
+DEFAULT_DATA = {
+    2015: {
+        'personal_allowance': 10600,
+        'starter_rate': None,
+        'basic_rate': {
+            'rate': 20,
+            'range_start': 0,
+            'range_end': 31785
+        },
+        'intermediate_rate': None,
+        'higher_rate': {
+            'rate': 40,
+            'range_start': 31786,
+            'range_end': 150000
+        },
+        'top_rate': None,
+    },
+    2016: {
+        'personal_allowance': 11000,
+        'starter_rate': None,
+        'basic_rate': {
+            'rate': 20,
+            'range_start': 0,
+            'range_end': 32000
+        },
+        'intermediate_rate': None,
+        'higher_rate': {
+            'rate': 40,
+            'range_start': 32001,
+            'range_end': 150000
+        },
+        'top_rate': None,
+    },
+    2017: {
+        'personal_allowance': 11500,
+        'starter_rate': None,
+        'basic_rate': {
+            'rate': 20,
+            'range_start': 0,
+            'range_end': 31500
+        },
+        'intermediate_rate': None,
+        'higher_rate': {
+            'rate': 40,
+            'range_start': 31501,
+            'range_end': 150000
+        },
+        'top_rate': None,
+    },
+    2018: {
+        'personal_allowance': 11850,
+        'starter_rate': {
+            'rate': 19,
+            'range_start': 0,
+            'range_end': 2000
+        },
+        'basic_rate': {
+            'rate': 20,
+            'range_start': 2001,
+            'range_end': 12150
+        },
+        'intermediate_rate': {
+            'rate': 21,
+            'range_start': 12151,
+            'range_end': 31580
+        },
+        'higher_rate': {
+            'rate': 40,
+            'range_start': 31581,
+            'range_end': 150000
+        },
+        'top_rate': {
+            'rate': 46,
+            'range_start': 150000,
+            'range_end': None
+        },
+    },
+}
