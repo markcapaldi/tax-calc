49a4ffb3b9558c68fd031f054555423f5040e0ca
Leon Larin <leonlarin@gmail.com>
2019-05-12 21:56:20 +0100

    Updated TaxBand to correctly calculate taxable band portions. Added tests.

--- CalculateTax.py
+++ CalculateTax.py
@@ -6,8 +6,6 @@ import json
 import locale
 import logging
 
-from functools import reduce
-
 from defaults import DEFAULT_DATA, TAX_DATA_FILE_NAME
 
 locale.setlocale(locale.LC_ALL, '')
@@ -17,7 +15,13 @@ def _validate_year(value):
     """
     Validating user input for year to be a valid year
     """
-    ivalue = int(value)
+    try:
+        ivalue = int(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(
+            "%s is an invalid year value" % value
+        )
+
     if (
         ivalue <= 0 or
         ivalue >= datetime.datetime.now().year + 1
@@ -32,7 +36,13 @@ def _validate_salary(value):
     """
     Validating user input for salary to be a positive integer
     """
-    ivalue = int(value)
+    try:
+        ivalue = int(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(
+            "%s is an invalid year value" % value
+        )
+
     if ivalue <= 0:
         raise argparse.ArgumentTypeError(
             "%s is an invalid salary value" % value
@@ -43,9 +53,10 @@ def _validate_salary(value):
 class IncomeTaxYearData:
     """
     Object containing tax data and calculated variables based on gross salary,
+    for a year
 
     init params:
-        year_tax_data: tax data for a particular year in a dict form
+        tax_data:     all available tax data
         gross_salary: salary used to calculate variable portions of tax
                       deductions like personal allowance and tax deductions
                       per band.
@@ -75,28 +86,55 @@ class IncomeTaxYearData:
             'Top Rate: {amount} @ {rate}%',
     }
 
-    def __init__(self, year_tax_data, gross_salary):
-        if year_tax_data is None:
+    def __init__(self, year=None, tax_data=None, gross_salary=None):
+        if year is None:
+            raise ValueError("year can't be null")
+
+        if tax_data is None:
             raise ValueError("year_tax_data can't be null")
 
         if gross_salary is None:
             raise ValueError("gross_salary can't be null")
 
+        if isinstance(year, int):
+            year = str(year)
+
+        if isinstance(tax_data, str):
+            tax_data = json.loads(tax_data)
+
+        try:
+            year_tax_data = tax_data[year]
+        except KeyError:
+            raise ValueError(
+                "Data for year %s is not available, please run command with "
+                "-h to see available options or try a different year. Data is "
+                "available for the following years: %s" %
+                (year, ', '.join([_year for _year in tax_data]))
+            )
+
         try:
             self.personal_allowance = year_tax_data['personal_allowance']
         except KeyError:
-            msg = ('Personal Allowance data is missing or corrupt, '
-                   'please try reseting tax data using -r parameter.')
+            msg = ('Personal Allowance data is missing, '
+                   'run command with -h to see available options.')
             raise ValueError(msg)
 
+        self.tax_data = tax_data
+        self.year = year
         self.gross_salary = gross_salary
         self.taxable_income = self.gross_salary - self.personal_allowance
 
-        for rate in self.BANDS:
-            if year_tax_data.get(rate) and year_tax_data[rate] is not None:
-                setattr(self, rate, TaxBand(year_tax_data, gross_salary, rate))
+        for band in self.BANDS:
+            if year_tax_data.get(band) and year_tax_data[band] is not None:
+                setattr(
+                    self, band, TaxBand(
+                        year_tax_data=year_tax_data,
+                        gross_salary=gross_salary,
+                        band=band
+                    )
+                )
             else:
-                setattr(self, rate, None)
+                setattr(self, band, None)
 
     def add(self, *args, **kwargs):
         """Add Tax Data for a year"""
@@ -147,35 +185,27 @@ class IncomeTaxYearData:
             return None
 
         if tax_band_obj.band_deduction:
-            if tax_band_obj.range_amount:
-                return self.BANDS.get(rate).format(
-                    amount=locale.currency(
-                        tax_band_obj.range_amount, grouping=True
-                    ),
-                    rate=(
-                        str(tax_band_obj.rate) + ' = ' +
-                        locale.currency(
-                            tax_band_obj.band_deduction,
-                            grouping=True
-                        )
-                    )
-                ) + '\n'
-        else:
-            return None
-
-    def get_tax_due_label(self):
-        return (
-            'Total Tax Due: ' +
-            locale.currency(
-                reduce(
-                    lambda x, y: x + y,
-                    [
-                        getattr(getattr(self, rate), 'band_deduction')
-                        for rate in self.BANDS
-                    ]
+            return self.BANDS.get(rate).format(
+                amount=locale.currency(
+                    tax_band_obj.range_amount, grouping=True
                 ),
+                rate=str(tax_band_obj.rate)
+            ) + ' = ' + locale.currency(
+                tax_band_obj.band_deduction,
                 grouping=True
             ) + '\n'
+
+        return None
+
+    def get_tax_due_label(self):
+        total_tax = 0
+        for band in self.BANDS:
+            if getattr(self, band):
+                total_tax += getattr(getattr(self, band), 'band_deduction')
+
+        return (
+            'Total Tax Due: ' +
+            locale.currency(total_tax, grouping=True) + '\n'
         )
 
     def get_breakdown(self):
@@ -195,7 +225,7 @@ class IncomeTaxYearData:
                 message += band_label
 
         if self.gross_salary:
-            message += self.get_tax_due_label()
+            message += '\n' + self.get_tax_due_label()
 
         return message
 
@@ -204,14 +234,72 @@ class TaxBand:
     """
     Tax Band object containing information for a particular tax band
     """
-    def __init__(self, year_tax_data, gross_salary, band):
+    def __init__(self, year_tax_data=None, gross_salary=None, band=None):
         logging.debug('Initializing TaxBand %s', band)
+
+        if gross_salary is None:
+            raise ValueError("gross_salary can't be null")
+
+        if year_tax_data is None:
+            raise ValueError("year_tax_data can't be null")
+
+        if band is None:
+            raise ValueError("band can't be null")
+
         self.name = band
-        self.rate = year_tax_data[band]['rate']
-        self.range_start = year_tax_data[band]['range_start']
-        self.range_end = year_tax_data[band]['range_end']
-        self.range_amount = 0
-        self.band_deduction = 0
+        try:
+            self.rate = year_tax_data[band]['rate']
+            self.range_start = year_tax_data[band]['range_start']
+            self.range_end = year_tax_data[band]['range_end']
+        except TypeError:
+            raise ValueError("This year does have the %s band" % band)
+
+        taxable_income = (
+            gross_salary - year_tax_data['personal_allowance']
+        )
+        band_deductions = {}
+        # We iterate through all the bands to calculate the deduction amount
+        # for this band
+        for band in IncomeTaxYearData.BANDS:
+            if year_tax_data.get(band) and year_tax_data[band] is not None:
+                # Save parameters of a band
+                band_range_start = year_tax_data[band]['range_start']
+                band_range_end = year_tax_data[band]['range_end']
+                band_rate = year_tax_data[band]['rate']
+
+                # Check that this is not an "infinite" band with no upper limit
+                if band_range_end:
+                    band_range_amount = band_range_end - band_range_start
+                    # We still have money to be taxed above this band
+                    if taxable_income > band_range_amount:
+                        band_deductions[band] = {
+                            'salary_part': band_range_amount,
+                            'tax_deduction': (
+                                (band_rate * band_range_amount) / 100.0
+                            )
+                        }
+                        taxable_income -= band_range_amount
+                    # There isn't any more money to be taxed in higher bands
+                    else:
+                        band_deductions[band] = {
+                            'salary_part': taxable_income,
+                            'tax_deduction': (
+                                (band_rate * taxable_income) / 100.0
+                            )
+                        }
+                        taxable_income = 0
+                # Infinite band like "Over 150,000"
+                else:
+                    band_deductions[band] = {
+                        'salary_part': taxable_income,
+                        'tax_deduction': (
+                            (band_rate * taxable_income) / 100.0
+                        )
+                    }
+                    taxable_income = 0
+
+        self.range_amount = band_deductions[self.name]['salary_part']
+        self.band_deduction = band_deductions[self.name]['tax_deduction']
 
     def __dict__(self):
         return {
@@ -239,10 +327,21 @@ def main():
         "-r", "--reset",
         help="reset tax data to defaults", action="store_true"
     )
-    parser.add_argument(
-        "-a", "--add-year",
-        help="add tax data for a year", action="store_true"
-    )
+
+    # parser.add_argument(
+    #     "-a", "--add-year",
+    #     help="add tax data for a year", action="store_true"
+    # )
+
+    # parser.add_argument(
+    #     "-e", "--edit-year",
+    #     help="edit tax data for a year", action="store_true"
+    # )
+
+    # parser.add_argument(
+    #     "-d", "--delete-year",
+    #     help="delete tax data for a year", action="store_true"
+    # )
 
     parser.add_argument(
         "tax_year",
@@ -261,12 +360,6 @@ def main():
         logging.info('Income Tax Data has been reset.')
         return
 
-    # Add Tax Data For a year
-    if args.add_year:
-        logging.info('Adding tax data to the data store.')
-        IncomeTaxYearData._add_tax_year_data()
-        return
-
     # Setting logging verbosity level
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
@@ -278,6 +371,7 @@ def main():
         logging.debug('Trying to load Tax Data from JSON.')
         with open(TAX_DATA_FILE_NAME) as json_file:
             tax_data = json.load(json_file)
+            json_file.close()
         logging.debug('Tax Data Loaded.')
     except json.decoder.JSONDecodeError:
         logging.error('Tax data is missing or corrupt, '
@@ -285,22 +379,27 @@ def main():
         return
 
     try:
-        data = IncomeTaxYearData(
-            tax_data[str(args.tax_year)],
-            args.gross_income
+        tax_data = IncomeTaxYearData(
+            args.tax_year, tax_data, args.gross_income
         )
     except KeyError:
         logging.error("Tax Data for year %s is not available, "
                       "you can add it using -a option." % args.tax_year)
         return
 
+    # # Add Tax Data For a year
+    # if args.add_year:
+    #     logging.info('Adding tax data to the data store.')
+    #     tax_data.add()
+    #     return
+
     logging.debug('Printing Tax Breakdown with salary breakdown')
     print(
-        "Tax Year: {year_from}-{year_to}\n"
+        "Tax Year: {year_from}-{year_to}\n\n"
         "{breakdown}".format(
             year_from=args.tax_year,
             year_to=args.tax_year + 1,
-            breakdown=data.get_breakdown()
+            breakdown=tax_data.get_breakdown()
         )
     )
     return
--- tests.py
+++ tests.py
@@ -6,9 +6,10 @@ import unittest
 
 
 from CalculateTax import (
-    IncomeTaxYearData,
-    _validate_year,
     _validate_salary,
+    _validate_year,
+    IncomeTaxYearData,
+    TaxBand,
 )
 from defaults import DEFAULT_DATA, TAX_DATA_FILE_NAME
 
@@ -26,7 +27,7 @@ class TestUtilFunctions(unittest.TestCase):
 
     def test_validate_year_very_invalid_year(self):
         year = 'foo'
-        with self.assertRaises(ValueError):
+        with self.assertRaises(argparse.ArgumentTypeError):
             _validate_year(year)
 
     def test_validate_salary_valid_salary(self):
@@ -41,7 +42,7 @@ class TestUtilFunctions(unittest.TestCase):
 
     def test_validate_salary_very_invalid_salary(self):
         salary = 'foo'
-        with self.assertRaises(ValueError):
+        with self.assertRaises(argparse.ArgumentTypeError):
             _validate_salary(salary)
 
 
@@ -53,7 +54,11 @@ class TestIncomeTaxYearData(unittest.TestCase):
 
     def test_init_valid_json_part_data(self):
         year = "2016"
-        year_data = IncomeTaxYearData(DEFAULT_DATA.get(year), 30000)
+        year_data = IncomeTaxYearData(
+            year=year,
+            tax_data=DEFAULT_DATA,
+            gross_salary=30000
+        )
         self.assertEqual(
             year_data.personal_allowance,
             DEFAULT_DATA[year]['personal_allowance']
@@ -80,9 +85,71 @@ class TestIncomeTaxYearData(unittest.TestCase):
         )
         self.assertEqual(year_data.personal_allowance, 11000)
 
+    def test_init_valid_json_part_data_as_str(self):
+        year = "2016"
+        year_data = IncomeTaxYearData(
+            year=year,
+            tax_data=json.dumps(DEFAULT_DATA),
+            gross_salary=30000
+        )
+        self.assertEqual(
+            year_data.personal_allowance,
+            DEFAULT_DATA[year]['personal_allowance']
+        )
+        self.assertEqual(
+            year_data.starter_rate,
+            DEFAULT_DATA[year]['starter_rate']
+        )
+        self.assertEqual(
+            year_data.basic_rate.__dict__(),
+            DEFAULT_DATA[year]['basic_rate']
+        )
+        self.assertEqual(
+            year_data.intermediate_rate,
+            DEFAULT_DATA[year]['intermediate_rate']
+        )
+        self.assertEqual(
+            year_data.higher_rate.__dict__(),
+            DEFAULT_DATA[year]['higher_rate']
+        )
+        self.assertEqual(
+            year_data.top_rate,
+            DEFAULT_DATA[year]['top_rate']
+        )
+        self.assertEqual(year_data.personal_allowance, 11000)
+
+    def test_init_valid_json_part_data_bad_data(self):
+        with self.assertRaises(ValueError) as cm:
+            IncomeTaxYearData(
+                year="2016",
+                tax_data={"2016": {
+                    'starter_rate': None,
+                    'basic_rate': {
+                        'rate': 20,
+                        'range_start': 0,
+                        'range_end': 32000
+                    },
+                    'intermediate_rate': None,
+                    'higher_rate': {
+                        'rate': 40,
+                        'range_start': 32001,
+                        'range_end': 150000
+                    },
+                    'top_rate': None
+                }},
+                gross_salary=30000
+            )
+        msg = ("Personal Allowance data is missing, run command with -h to "
+               "see available options.")
+        self.assertEqual(cm.exception.args[0], msg)
+
     def test_init_valid_json_full_data(self):
         year = "2018"
-        year_data = IncomeTaxYearData(DEFAULT_DATA.get(year), 30000)
+        year_data = IncomeTaxYearData(
+            year=year,
+            tax_data=DEFAULT_DATA,
+            gross_salary=30000
+        )
         self.assertEqual(
             year_data.personal_allowance,
             DEFAULT_DATA[year]['personal_allowance']
@@ -111,70 +178,237 @@ class TestIncomeTaxYearData(unittest.TestCase):
 
     def test_init_unexpected_valid_json(self):
         with self.assertRaises(ValueError) as cm:
-            IncomeTaxYearData({'foo': 'bar'}, 30000)
+            IncomeTaxYearData(
+                year=2016,
+                tax_data={'foo': 'bar'},
+                gross_salary=30000
+            )
 
-        msg = ('Personal Allowance data is missing or corrupt, please '
-               'try reseting tax data using -r parameter.')
+        msg = ('Data for year 2016 is not available, please run command with '
+               '-h to see available options or try a different year. Data is '
+               'available for the following years: foo')
+        self.assertEqual(cm.exception.args[0], msg)
+
+    def test_init_year_is_null(self):
+        with self.assertRaises(ValueError) as cm:
+            IncomeTaxYearData(year=None, tax_data={}, gross_salary=32000)
+        msg = "year can't be null"
         self.assertEqual(cm.exception.args[0], msg)
 
     def test_init_year_tax_data_is_null(self):
         with self.assertRaises(ValueError) as cm:
-            IncomeTaxYearData(None, 30000)
+            IncomeTaxYearData(year=2016, tax_data=None, gross_salary=30000)
         msg = "year_tax_data can't be null"
         self.assertEqual(cm.exception.args[0], msg)
 
     def test_init_gross_salary_is_null(self):
         with self.assertRaises(ValueError) as cm:
-            IncomeTaxYearData(2015, None)
+            IncomeTaxYearData(year=2016, tax_data={}, gross_salary=None)
         msg = "gross_salary can't be null"
         self.assertEqual(cm.exception.args[0], msg)
 
     # Not implemented method test stubs
 
     def test_add_not_implemented(self):
-        year_data = IncomeTaxYearData(DEFAULT_DATA.get("2016"), 30000)
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
         with self.assertRaises(NotImplementedError):
             year_data.add()
 
     def test_edit_not_implemented(self):
-        year_data = IncomeTaxYearData(DEFAULT_DATA.get("2016"), 30000)
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
         with self.assertRaises(NotImplementedError):
             year_data.edit()
 
     def test_delete_not_implemented(self):
-        year_data = IncomeTaxYearData(DEFAULT_DATA.get("2016"), 30000)
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
         with self.assertRaises(NotImplementedError):
             year_data.delete()
 
     def test_save_not_implemented(self):
-        year_data = IncomeTaxYearData(DEFAULT_DATA.get("2016"), 30000)
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
         with self.assertRaises(NotImplementedError):
             year_data.save()
 
     # Label functions tests
 
     def test_get_gross_salary_label(self):
-        year_data = IncomeTaxYearData(DEFAULT_DATA.get("2016"), 30000)
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
         label = year_data.get_gross_salary_label()
         self.assertEqual(label, 'Gross Salary: £30,000.00\n')
 
     def test_get_personal_allowance_label(self):
-        year_data = IncomeTaxYearData(DEFAULT_DATA.get("2016"), 30000)
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
         label = year_data.get_personal_allowance_label()
         self.assertEqual(year_data.personal_allowance, 11000)
         self.assertEqual(label, 'Personal Allowance: £11,000.00\n')
 
     def test_get_taxable_income_label(self):
-        year_data = IncomeTaxYearData(DEFAULT_DATA.get("2016"), 30000)
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
         label = year_data.get_taxable_income_label()
         self.assertEqual(label, 'Taxable Income: £19,000.00\n')
 
-    # Various tests
-    def test_load_json_file(self):
+    def test_get_band_label_no_rate(self):
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
+        label = year_data.get_band_label('starter_rate')
+        self.assertEqual(label, None)
+
+    def test_get_band_label(self):
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
+        label = year_data.get_band_label('basic_rate')
+        self.assertEqual(label, "Basic Rate: £19,000.00 @ 20% = £3,800.00\n")
+
+    def test_get_band_label_over_highest(self):
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
+        label = year_data.get_band_label('higher_rate')
+        self.assertEqual(label, None)
+
+    def test_get_tax_due_label(self):
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=30000
+        )
+        label = year_data.get_tax_due_label()
+        self.assertEqual(label, "Total Tax Due: £3,800.00\n")
+
+    def test_get_breakdown_short(self):
+        year_data = IncomeTaxYearData(
+            year=2016, tax_data=DEFAULT_DATA, gross_salary=25000
+        )
+        label = year_data.get_breakdown()
+        self.assertEqual(
+            label,
+            "Gross Salary: £25,000.00\n\n"
+            "Personal Allowance: £11,000.00\n\n"
+            "Taxable Income: £14,000.00\n\n"
+            "Basic Rate: £14,000.00 @ 20% = £2,800.00\n\n"
+            "Total Tax Due: £2,800.00\n"
+        )
+
+    # Other tests
+    def test_reset_tax_data(self):
         IncomeTaxYearData._reset_tax_data()
         with open(TAX_DATA_FILE_NAME) as json_file:
             tax_data = json.load(json_file)
         self.assertEqual(tax_data, json.loads(json.dumps(DEFAULT_DATA)))
+
+
+class TestTaxBand(unittest.TestCase):
+    def setUp(self):
+        self.short_year_data = {
+            'personal_allowance': 11000,
+            'starter_rate': None,
+            'basic_rate': {
+                'rate': 20,
+                'range_start': 0,
+                'range_end': 32000
+            },
+            'intermediate_rate': None,
+            'higher_rate': {
+                'rate': 40,
+                'range_start': 32001,
+                'range_end': 150000
+            },
+            'top_rate': None
+        }
+        self.full_year_data = {
+            'personal_allowance': 11850,
+            'starter_rate': {
+                'rate': 19,
+                'range_start': 0,
+                'range_end': 2000
+            },
+            'basic_rate': {
+                'rate': 20,
+                'range_start': 2001,
+                'range_end': 12150
+            },
+            'intermediate_rate': {
+                'rate': 21,
+                'range_start': 12151,
+                'range_end': 31580
+            },
+            'higher_rate': {
+                'rate': 40,
+                'range_start': 31581,
+                'range_end': 150000
+            },
+            'top_rate': {
+                'rate': 46,
+                'range_start': 150000,
+                'range_end': None
+            }
+        }
+
+    def test_init_year_tax_data_is_null(self):
+        with self.assertRaises(ValueError) as cm:
+            TaxBand(
+                year_tax_data=None, gross_salary=30000, band='starter_rate'
+            )
+        msg = "year_tax_data can't be null"
+        self.assertEqual(cm.exception.args[0], msg)
+
+    def test_init_gross_salary_is_null(self):
+        with self.assertRaises(ValueError) as cm:
+            TaxBand(
+                year_tax_data={}, gross_salary=None, band='starter_rate'
+            )
+        msg = "gross_salary can't be null"
+        self.assertEqual(cm.exception.args[0], msg)
+
+    def test_init_band_is_null(self):
+        with self.assertRaises(ValueError) as cm:
+            TaxBand(
+                year_tax_data={}, gross_salary=30000, band=None
+            )
+        msg = "band can't be null"
+        self.assertEqual(cm.exception.args[0], msg)
+
+    def test_short_year_data_tax_calculation(self):
+        band = TaxBand(
+            year_tax_data=self.short_year_data,
+            gross_salary=30000,
+            band='basic_rate'
+        )
+        self.assertEqual(band.range_amount, 19000)
+        self.assertEqual(band.band_deduction, 3800.0)
+
+    def test_full_year_data_tax_calculation(self):
+        band = TaxBand(
+            year_tax_data=self.full_year_data,
+            gross_salary=30000,
+            band='basic_rate'
+        )
+        self.assertEqual(band.range_amount, 10149)
+        self.assertEqual(band.band_deduction, 2029.8)
+
+    def test_short_year_data_tax_calculation_bad_band(self):
+        with self.assertRaises(ValueError) as cm:
+            TaxBand(
+                year_tax_data=self.short_year_data,
+                gross_salary=40000,
+                band='starter_rate'
+            )
+        msg = "This year does have the starter_rate band"
+        self.assertEqual(cm.exception.args[0], msg)
 
 
 if __name__ == '__main__':
